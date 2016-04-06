aee9da680578b0dd23247616e0e61ce6a65c180d
vergevsky <vergevsky@gmail.com>
2016-04-06 08:43:54 +0500

    link on XML

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if (window.XMLHttpRequest) {
 } else {
 	request = new ActiveXObject("Microsoft.XMLHHTP");
 }
-request.open('GET', 'http://xmltv.s-tv.ru/xmlfull.php?prg=1459181329&sh=0');
+request.open('GET', 'RTR.xml');
 request.onreadystatechange = function(){
 	if ((request.readyState===4) && (request.Status===200)) {
 		console.log(request.responseXML);
