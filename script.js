b56d488ba039d7234b040ac20c9b10d86654bac3
vergevsky <vergevsky@gmail.com>
2016-04-06 15:12:57 +0500

    firstchilde

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ if (window.XMLHttpRequest) {
 request.open('GET', 'RTR.xml');
 request.onreadystatechange = function() {
     if ((request.readyState===4) && (request.status===200)) {
-        console.log(request.responseXML.getElementsByTagName('display-name')[1].firstChild.nodeValue);
+        console.log(request.responseXML.getElementsByTagName('display-name')[1].nodeValue);
         
         var items = request.responseXML.getElementsByTagName('display-name');
         var output = '<ul>';
         for (var i = 0; i < items.length; i++) {
-            output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+            output += '<li>' + items[i].nodeValue + '</li>';
         }
         output += '</ul>';
         document.getElementById('update').innerHTML = output;
